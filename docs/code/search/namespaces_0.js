dacdf4a0fbd72e9018dbcae3ab1506accbb89388
Marcos Abraham Hernandez Bravo <marcos.her1993@outlook.com>
2016-09-25 23:03:47 -0300

    Se añade documentación de código.

---
+++ docs/code/search/namespaces_0.js
@@ -0,0 +1,9 @@
+var searchData=
+[
+  ['core',['Core',['../namespace_wheel_1_1_core.html',1,'Wheel']]],
+  ['customattributes',['CustomAttributes',['../namespace_wheel_1_1_core_1_1_custom_attributes.html',1,'Wheel::Core']]],
+  ['development',['Development',['../namespace_wheel_1_1_core_1_1_custom_attributes_1_1_development.html',1,'Wheel::Core::CustomAttributes']]],
+  ['exceptions',['Exceptions',['../namespace_wheel_1_1_core_1_1_exceptions.html',1,'Wheel::Core']]],
+  ['object',['Object',['../namespace_wheel_1_1_core_1_1_custom_attributes_1_1_object.html',1,'Wheel::Core::CustomAttributes']]],
+  ['wheel',['Wheel',['../namespace_wheel.html',1,'']]]
+];
---
+++ docs/code/search/properties_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['descripcion',['Descripcion',['../class_wheel_1_1_core_1_1_exceptions_1_1_argumento_invalido.html#a393f3460ebf8ce03e190082a7049222d',1,'Wheel::Core::Exceptions::ArgumentoInvalido']]]
+];
