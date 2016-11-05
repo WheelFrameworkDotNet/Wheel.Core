e8fbc313ffaa50bb27d028816021329aaed381a2
Marcos Abraham Hernandez Bravo <marcos.her1993@outlook.com>
2016-11-05 20:58:47 -0300

    Clean

--- docs/code/search/all_3.js
+++
@@ -1,8 +0,0 @@
-var searchData=
-[
-  ['enumeracion',['Enumeracion',['../class_wheel_1_1_core_1_1_enumeracion.html',1,'Wheel::Core']]],
-  ['enumeracion',['Enumeracion',['../class_wheel_1_1_core_1_1_enumeracion.html#a7c59740c0584d078d43c2376dab08d0e',1,'Wheel::Core::Enumeracion']]],
-  ['enumeracion_3c_20string_2c_20argumentoinvalido_20_3e',['Enumeracion&lt; string, ArgumentoInvalido &gt;',['../class_wheel_1_1_core_1_1_enumeracion.html',1,'Wheel::Core']]],
-  ['equals',['Equals',['../class_wheel_1_1_core_1_1_enumeracion.html#a6d8d2346b3addb55d916093190079f71',1,'Wheel::Core::Enumeracion']]],
-  ['esinmutable',['EsInmutable',['../class_wheel_1_1_core_1_1_custom_attributes_1_1_object_1_1_inmutable.html#a2a0f71f46bc90637caf2eed366f478b1',1,'Wheel::Core::CustomAttributes::Object::Inmutable']]]
-];
--- docs/code/search/properties_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['descripcion',['Descripcion',['../class_wheel_1_1_core_1_1_exceptions_1_1_argumento_invalido.html#a393f3460ebf8ce03e190082a7049222d',1,'Wheel::Core::Exceptions::ArgumentoInvalido']]]
-];
