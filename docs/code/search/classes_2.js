5ecfa0ff378471abfc9eb68256473f63ec6993a2
Marcos Abraham Hernandez Bravo <marcos.her1993@outlook.com>
2016-11-05 20:55:35 -0300

    Automatico.

---
+++ docs/code/search/classes_2.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['enumeracion',['Enumeracion',['../class_wheel_1_1_core_1_1_enumeracion.html',1,'Wheel::Core']]],
+  ['enumeracion_3c_20string_2c_20argumentoinvalido_20_3e',['Enumeracion&lt; string, ArgumentoInvalido &gt;',['../class_wheel_1_1_core_1_1_enumeracion.html',1,'Wheel::Core']]]
+];
