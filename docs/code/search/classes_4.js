3db2b5d71f308881381e4f75eb558d258fe8a4b0
Marcos Abraham Hernandez Bravo <marcos.her1993@outlook.com>
2016-11-05 22:01:48 -0300

    Versión Inicial.

---
+++ docs/code/search/classes_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['noimplementado',['NoImplementado',['../class_wheel_1_1_core_1_1_custom_attributes_1_1_development_1_1_no_implementado.html',1,'Wheel::Core::CustomAttributes::Development']]]
+];
---
+++ test/EnumeracionUnitTests/Enumeracion_EqualsUT.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Wheel.Core.Entities;
+
+namespace Wheel.Core
+{
+    public partial class EnumeracionUT2
+    {
+        [TestMethod]
+        public void CasoFeliz()
+        {
+            Assert.IsTrue(Estado.Nulo.Equals(Estado.Nulo), "{ param1: Estado.Nulo, param2: Estado.Nulo }");
+            Assert.IsTrue(Estado.Activado.Equals(Estado.ActivadoDuplicado), "{ param1: Estado.Activado, param2: Estado.ActivadoDuplicado }");
+        }
+        
+        [TestMethod]
+        public void ObjetosIguales_1_1_2()
+        {
+            Assert.IsTrue(Estado.Activado.Equals(Estado.Activado), "{ param1: Estado.Activado, param2: Estado.Activado }");
+        }
+
+        [TestMethod]
+        public void CasoDesigualdad()
+        {
+            Assert.IsFalse(Estado.Bloqueado.Equals(Estado.Activado), "{ param1: Estado.Bloqueado, param2: Estado.Activado }");
+            Assert.IsFalse(Estado.Activado.Equals(Estado.Bloqueado), "{ param1: Estado.Activado, param2: Estado.Bloqueado }");
+            Assert.IsFalse(Estado.Nulo.Equals(Estado.Activado), "{ param1: Estado.Nulo, param2: Estado.Activado }");
+            Assert.IsFalse(Estado.Activado.Equals(Estado.Nulo), "{ param1: Estado.Activado, param2: Estado.Nulo }");
+        }
+
+        [TestMethod]
+        public void CasoNulo()
+        {
+            Assert.IsFalse(Estado.Activado.Equals(null));
+        }
+    }
+}
